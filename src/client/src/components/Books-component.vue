e5eb98ded38c67eac8020f1c4044840ba77e817d
Эмиль Шаймарданов <shaimardanov_e@ufanet.ru>
2022-03-11 18:44:26 +0500

    chg: bootstrap link

--- src/client/src/components/Books-component.vue
+++ src/client/src/components/Books-component.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
+
+
     <ul v-if="books && books.length">
       <li v-for="book of books" :key="book">
         <p><strong>{{ book.id }}</strong></p>
         <p>{{ book.title }}</p>
         <p>{{ book.count }}</p>
         <br>
+        <button class="btn btn-primary">Knopka 1</button>
+        <button class="btn btn-primary">Knopka 2</button>
       </li>
     </ul>
     <ul v-if="errors && errors.length">
